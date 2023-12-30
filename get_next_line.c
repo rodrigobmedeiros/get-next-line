3d0bb3a90f92d8b463d4c65676147abbbef3c930
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2023-12-30 17:16:54 -0300

    Use a wrapper to validate the file descriptor before call the logic itself

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,13 @@
 /*   By: robernar <robernar@student.42.rj>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/05 06:31:27 by robernar          #+#    #+#             */
-/*   Updated: 2023/12/30 15:46:09 by robernar         ###   ########.fr       */
+/*   Updated: 2023/12/30 16:32:38 by robernar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "get_next_line.h"
 #include <stdio.h>
 
-int	validate_fd(int fd)
+static int	validate_fd(int fd)
 {
 	char	buffer[1];
 	int	b_read;
@@ -25,17 +25,16 @@ int	validate_fd(int fd)
 	return (1);
 }
 
-char	*get_next_line(int fd)
+static char	*get_next_line_logic(int fd)
 {
-	int		n_bytes_read;
+	int		nb_read;
 	static char	*buffer;
 	char		*line;
 	int		len_line;
 
-	if (!validate_fd(fd))
-		return (NULL);
-	n_bytes_read = 1;
-	while ((!line && *buffer) || n_bytes_read > 0)
+	nb_read = 1;
+	line = "";
+	while ((!line && *buffer) || nb_read > 0)
 	{
 		len_line = has_breakline(buffer);
 		if (len_line)
@@ -47,25 +46,39 @@ char	*get_next_line(int fd)
 		else
 		{
 			buffer = resize_buffer(buffer);
-			n_bytes_read = read(fd, buffer + ft_strlen(buffer), BUFFER_SIZE);
+			nb_read = read(fd, buffer + ft_strlen(buffer), BUFFER_SIZE);
 		}
 	}
 	free(buffer);
 	return (NULL);
 }
-/*
+
+/**
+ * Here the get_next_line is a wrapper that validate the fd checking if it's valid and
+ * not empty. If this validation is OK, the get_next_line_logic is called, extracting the
+ * line.
+ * 
+ * @param fd file descriptor to read.
+ * @return the next valid line of a file or NULL.
+ */
+char	*get_next_line(int fd)
+{
+	if (validate_fd(fd))
+		return get_next_line_logic(fd);
+	return (NULL);
+}
+
 int main()
 {
 	int	fd;
-	char	*str;
-	
-	fd = open("empty", O_RDONLY);
+	char	str[1];
+	int	n_bytes;
 
-	str = get_next_line(fd);
-	if (str)
-		printf("%s\n", str);
-	else
-		printf("(null)\n");
-	close(fd);
+	fd = open("empty", O_RDONLY);
+	n_bytes = read(fd, str, 1);
+	printf("%d\n", n_bytes);
+	n_bytes = read(fd, str, 1);
+        printf("%d\n", n_bytes);
+	n_bytes = read(fd, str, 1);
+        printf("%d\n", n_bytes);
 }
-*/
