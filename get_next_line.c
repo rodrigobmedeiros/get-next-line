a475c47044217b85238c18af117b74fe47ab9791
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2023-12-30 15:47:54 -0300

    Add few adjustments

--- get_next_line.c
+++ get_next_line.c
@@ -6,20 +6,34 @@
 /*   By: robernar <robernar@student.42.rj>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/05 06:31:27 by robernar          #+#    #+#             */
-/*   Updated: 2023/12/28 06:38:37 by robernar         ###   ########.fr       */
+/*   Updated: 2023/12/30 15:46:09 by robernar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "get_next_line.h"
 #include <stdio.h>
 
+int	validate_fd(int fd)
+{
+	char	buffer[1];
+	int	b_read;
+
+	if (fd < 0)
+		return (0);
+	b_read = read(fd, buffer, 1);
+	if (b_read <= 0)
+		return (0);
+	return (1);
+}
+
 char	*get_next_line(int fd)
 {
-	int			n_bytes_read;
+	int		n_bytes_read;
 	static char	*buffer;
 	char		*line;
-	int			len_line;
+	int		len_line;
 
-	line = "";
+	if (!validate_fd(fd))
+		return (NULL);
 	n_bytes_read = 1;
 	while ((!line && *buffer) || n_bytes_read > 0)
 	{
@@ -45,16 +59,13 @@ int main()
 	int	fd;
 	char	*str;
 	
-	fd = open("teste.txt", O_RDONLY);
+	fd = open("empty", O_RDONLY);
+
 	str = get_next_line(fd);
-	printf("%s", str);
-	while (str)
-	{
-		free(str);
-		str = get_next_line(fd);
-		if (str)
-			printf("%s", str);
-	}
+	if (str)
+		printf("%s\n", str);
+	else
+		printf("(null)\n");
 	close(fd);
 }
 */
